e430ffc7a7c76ce794a749012590bf0b863a97ef
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-03-24 16:08:20 +0100

    created chalk

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk")
+const names = ["Pedro", "Hugo", "Rodrigo", "Filipe"];
+console.log(chalk.blue(names[0]));
+console.log(chalk.red(names[1]));
+console.log(chalk.green(names[2]));
+console.log(chalk.yellow(names[3]));
